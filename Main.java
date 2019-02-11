2eff8c144e3b7ec8619e4757c2324ea74185c6c6
Hokchris <christian.honkanen@gmail.com>
2019-02-11 12:09:18 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,422 +1,182 @@
-import lejos.hardware.BrickFinder;
-import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.RegulatedMotor;
-import lejos.robotics.SampleProvider;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.EV3GyroSensor;
-
 
 public class Main {
-	public static void main(String[] args) {
-		
-		GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
-		String s;
-		
-		// First touch sensor
-//		EV3TouchSensor touchSensor1 = new EV3TouchSensor(SensorPort.S1);
-//		SensorMode touch1 = touchSensor1.getTouchMode();
-//		float[] sample1 = new float[touch1.sampleSize()];
-		
-		// Second touch sensor
-		//EV3TouchSensor touchSensor2 = new EV3TouchSensor(SensorPort.S2);
-		//SensorMode touch2 = touchSensor2.getTouchMode();
-		//float[] sample2 = new float[touch2.sampleSize()];
-		
-		// Color sensor
-		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
-		SampleProvider colorProvider = colorSensor.getRGBMode();
-		float[] colorSample = new float[3];
-		
-		// Ultrasonic sensor
-//		EV3UltrasonicSensor ultraSensor = new EV3UltrasonicSensor(SensorPort.S2);
-//		SampleProvider ultraProvider = ultraSensor.getDistanceMode();
-//		float[] ultraSample = new float[ultraProvider.sampleSize()];
-		
-		
-		// Large wheel motors
-		RegulatedMotor mA = new EV3LargeRegulatedMotor(MotorPort.A);
-		RegulatedMotor mB = new EV3LargeRegulatedMotor(MotorPort.B);
-		
-		// Medium rotating motor
-//		RegulatedMotor mC = new EV3MediumRegulatedMotor(MotorPort.B);
-		
-
-		double kP = 800;
-		double kI = 600;
-		double kD = 15;
-		double offset = 0.210;
-		double tP = 180;		
-		double integral = 0;
-		double lastError = 0;
-		double derivative = 0;
-		double initialTime = 0;
-		
-		while(true) {
-			
-			// Implementation of PID control.
-			
-			// We want to reset the integral back to 0 every time the program goes through a loop, 
-			// as the integral is continuosly added to every run, and the speed of the wheels is partly decided by this value.
-			// If the value was not reset, the integral would keep increasing in a linear fashion, and we would go to max speed on the wheels.
 	
-			initialTime = System.currentTimeMillis();
-			
-			colorProvider.fetchSample(colorSample, 0);
-			mA.synchronizeWith(new RegulatedMotor[]{mB});
+	static RegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+	static RegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
+
+	static EV3TouchSensor touch1 = new EV3TouchSensor(SensorPort.S1);
+	static SensorMode t1 = touch1.getTouchMode();
+	static float[] tSample1 = new float[t1.sampleSize()];
+	
+	static EV3TouchSensor touch2 = new EV3TouchSensor(SensorPort.S2);
+	static SensorMode t2 = touch2.getTouchMode();
+	static float[] tSample2 = new float[t2.sampleSize()];
+
+	static EV3ColorSensor cSensor = new EV3ColorSensor(SensorPort.S4);
+	static SampleProvider cProvider = cSensor.getRedMode();
+	static float[] cSample = new float[1];
+
+	static Wheel wLeft = WheeledChassis.modelWheel(motorA, 3).offset(4.155);
+	static Wheel wRight = WheeledChassis.modelWheel(motorB, 3).offset(-4.155);
+
+	static Chassis chassis = new WheeledChassis(new Wheel[]{wLeft, wRight}, WheeledChassis.TYPE_DIFFERENTIAL);
+	static MovePilot pilot = new MovePilot(chassis);
+
+	static boolean isRed = false;
+
+	public static void main(String[] args) {
+
+		while(true) {
+
+			pilot.setLinearSpeed(15);
+			pilot.travel(43);
+//			Delay.msDelay(1000);
+
+			pilot.setAngularSpeed(45);
+			pilot.rotate(90);
+			pilot.travel(13);
+//			Delay.msDelay(1000);
+
+			pilot.arc(-15, 120);
+			pilot.arc(0, 45);
+
+			pilot.travel(23);
+			pilot.rotate(-64);
+			pilot.setLinearSpeed(15);
+			pilot.travel(31);
+
+			t1.fetchSample(tSample1, 0);
+			t2.fetchSample(tSample2, 0);
+			cProvider.fetchSample(cSample, 0);
 
 			
-			float redValue = colorSample[0];
-			float greenValue = colorSample[1];
-			float blueValue = colorSample[2];
-			
-			double totalError = (redValue - offset) + (greenValue - offset) + (blueValue - offset);
-			double averageError = totalError/3;
-			
-			// || Math.abs((lastError - averageError)) > Math.abs(lastError)
-			if (lastError == 0 || Math.abs((lastError - averageError)) > Math.abs(lastError)) {
-				integral = 0;
+			System.out.print("Test0");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+
+			System.out.println(tSample1[0] != 0);
+			System.out.println(tSample2[0] != 0);
+
+			if ((tSample1[0] != 0) || (tSample2[0] != 0)) {
+
+//				System.out.print("Test1");
+
+				if ((cSample[0]) > 0.5) {
+//					System.out.print("Test2");
+					isRed = true; 
+				}
+
+				touchEnd(isRed);				
+
+			} else {
+
+				Sound.buzz();
+				Delay.msDelay(500);
+				Sound.buzz();
+
 			}
-			
-			double dT = System.currentTimeMillis() - initialTime;
-			integral = (1/8 * integral) + (averageError * dT);
-			derivative = averageError - lastError;
-			
-			double turn = 1.0 * ((kP * averageError) + (kI * integral) + (kD * derivative));
 
-			double powerLeft = tP - turn;
-			double powerRight = tP + turn;
-			
-			lastError = averageError;
-			
-			mA.startSynchronization();
-			mA.setSpeed((int)powerLeft);
-			mB.setSpeed((int)powerRight);
-			mA.forward();
-			mB.forward();
-			mA.endSynchronization();
+			Delay.msDelay(5000);
+//			break;
 
 		}
+
+	}
+	
+
+	static private void touchEnd(boolean color) {
+
+		// true = red
+		// false = green
+
+		System.out.print("Test3");
+
+		if (color == true) {
+			
+			// BETWEEN CYLINDER AND BARRIER
+			
+			Sound.beep();
+			Delay.msDelay(100);
+			
+			pilot.travel(-45);
+			pilot.rotate(-48); // MAKE 48
+			pilot.travel(59.4);
+			pilot.setAngularSpeed(40);
+			pilot.arc(-15, 135);
+			
+		} else {
+			
+			// BETWEEN CYLINDERS
+			
+			Sound.twoBeeps();
+			Delay.msDelay(100);
+			
+			pilot.travel(-45);
+			pilot.rotate(-48); // MAKE 48
+			pilot.travel(79.6);
+			pilot.setAngularSpeed(40);
+			pilot.arc(-15, 135);
+			
+		}
+		
 	}
 	
 	
-static void runMotors(RegulatedMotor motor1, RegulatedMotor motor2, float[] sample) {
-
-	// If motor is moving/not moving and red is detected, stop the motors
-	// If motor is moving/not moving and black is detected, run motor.
-	// If motor is running and white is detected, slow down.
-	// If unknown color is found, go to a halt.
+	double[] locationProbability = new double[37];
+	Arrays.fill(locationProbability, (1/37));
 	
-	motor1.synchronizeWith(new RegulatedMotor[]{motor2});
+	double moveWork = 0.98;
+	double sensorWork = 0.98;
 	
-	if ( 	(sample[0] > 0.200f) && (sample[0] < 0.300f)
-			&&	(sample[1] > 0.020f) && (sample[1] < 0.060f)
-			&& 	(sample[2] > 0.020f) && (sample[2] < 0.060f)
-		) {
+	// Blue = TRUE, White = FALSE
+	// 1 unit = 1.cm
+	// 2W, 3B, 1W, 2B, 2W, 3B, 1W, 2B, 2W, 3B, 2W, 3B, 1W, 2B, 2W, 3B, 1W, 2B
+	// 37 grids
+	
+	boolean[] colorArray = new boolean[
+	                                   (false, false, true, true, true, false, true, true, false, false, 
+	                                	true, true, true, false, true, true, false, false, true, true, true,
+	                                	false, false, true, true, true, false, true, true, false, false
+	                                	true, true, true, false, true, true)
+	                                   ];
+	
+	// HASHMAP
+	HashMap map = new HashMap();
+	
+	
+	static private void localisation() {
+	
+		double nFactor = 0;
 		
-			// RED
-			motor1.startSynchronization();
-			motor1.stop();
-			motor2.stop();
-			motor1.endSynchronization();
-			
-		} else if ( (sample[0] > 0.020f) && (sample[0] < 0.065f)
-				&& 	(sample[1] > 0.020f) && (sample[1] < 0.065f)
-				&& 	(sample[2] > 0.020f) && (sample[2] < 0.065f)
-		) {
-			
-			// BLACK
-			motor1.startSynchronization();
-			motor1.setSpeed(18);
-			motor1.forward();
-			motor2.setSpeed(180);
-			motor2.forward();
-			motor1.endSynchronization();
-			
-		} else if ((sample[0] > 0.150f) && (sample[0] < 0.400f)
-				&& 	(sample[1] > 0.150f) && (sample[1] < 0.400f)
-				&& 	(sample[2] > 0.150f) && (sample[2] < 0.400f)
-		) {
-			
-			// WHITE
-			motor1.startSynchronization();
-			motor1.setSpeed(180);
-			motor1.forward();
-			motor2.setSpeed(18);
-			motor2.forward();
-			motor1.endSynchronization();
-			
-		} else if ((sample[0] > 0.065f) && (sample[0] < 0.150f)
-				&& 	(sample[1] > 0.065f) && (sample[1] < 0.150f)
-				&& 	(sample[2] > 0.065f) && (sample[2] < 0.150f)
-			) {
-			
-			// GREY
-			motor1.startSynchronization();
-			motor1.setSpeed(180);
-			motor1.forward();
-			motor2.setSpeed(180);
-			motor2.forward();
-			motor1.endSynchronization();
-		} else {
-			
-			// UNKNOWN
-			motor1.startSynchronization();
-			motor1.setSpeed(90);
-			motor1.forward();
-			motor2.setSpeed(90);
-			motor2.forward();
-			motor1.endSynchronization();
-		}
+	}
 	
+}
 	
-//	if (color == "red") {
-////		System.out.println("Found red, stopping");
-//		motor1.startSynchronization();
-//		motor1.stop();
-//		motor2.stop();
-//		motor1.endSynchronization();
-////		Delay.msDelay(100);
+//	public static void moveSquare() {
 //		
-//	} else if (color == "black") {
-////		System.out.println("Found black, running");
-//		motor1.startSynchronization();
-//		motor1.setSpeed(24);
-//		motor1.forward();
-//		motor2.setSpeed(240);
-//		motor2.forward();
-//		motor1.endSynchronization();
-//		
-//	} else if (color == "white") {
-////		System.out.println("Found white, slowing down");
-//		motor1.startSynchronization();
-//		motor1.setSpeed(240);
-//		motor1.forward();
-//		motor2.setSpeed(24);
-//		motor2.forward();
-//		motor1.endSynchronization();
-////		Delay.msDelay(500);
-//		
-//	} else if (color == "grey") {	
-////		System.out.println("Found unknown color, spinning to a halt");
-//		motor1.startSynchronization();
-//		motor1.setSpeed(240);
-//		motor1.forward();
-//		motor2.setSpeed(240);
-//		motor2.forward();
-//		motor1.endSynchronization();
-////		Delay.msDelay(1000); 
-//		
-//	} else {
-//		motor1.startSynchronization();
-//		motor1.setSpeed(120);
-//		motor1.forward();
-//		motor2.setSpeed(120);
-//		motor2.forward();
-//		motor1.endSynchronization();
+//		for (int i = 0; i < 4; i++) {
+//			pilot.setLinearSpeed(30);
+//			pilot.travel(10);
+//			pilot.setAngularSpeed(6);
+//			pilot.rotate(90, true);
+//			
+//			Delay.msDelay(4000);
+//			if (i == 3) {
+//				Delay.msDelay(10000);
+//			}
+//		}
 //		
 //	}
-		
-}
-
-// NOT USING ATM
-static String determineColor(float[] sample) {
-	
-	String color = "";
-	// ////////// VERY LARGE RANGES, MIGHT GIVE UNWANTED RESULTS //////////////
-	// RED = Red between 0.2 and 0.3, Green and blue between 0.05 / 0.06 and 0.03 and 0.04
-	// BLACK = Red between 0.055 and 0.65, Green and blue between 0.050 / 0.060 and 0.050 and 0.060
-	// WHITE = Red between 0.2 and 0.3, Green and blue between 0.2 / 0.3 and 0.2 and 0.3
-	// If not in above ranges, color is unknown. This may cause problems later if a slightly different hue of certain color is found.
-	
-	
-	if ( 	(sample[0] > 0.200f) && (sample[0] < 0.300f)
-			&&	(sample[1] > 0.020f) && (sample[1] < 0.060f)
-			&& 	(sample[2] > 0.020f) && (sample[2] < 0.060f)
-		) {
-			color = "red";
-//			System.out.println("DETECTING RED");
-			
-		} else if ( (sample[0] > 0.020f) && (sample[0] < 0.065f)
-				&& 	(sample[1] > 0.020f) && (sample[1] < 0.065f)
-				&& 	(sample[2] > 0.020f) && (sample[2] < 0.065f)
-		) {
-			color = "black";
-//			System.out.println("DETECTING BLACK");
-			
-		} else if ((sample[0] > 0.150f) && (sample[0] < 0.400f)
-				&& 	(sample[1] > 0.150f) && (sample[1] < 0.400f)
-				&& 	(sample[2] > 0.150f) && (sample[2] < 0.400f)
-		) {
-			color = "white";
-//			System.out.println("DETECTING WHITE");
-			
-		} else if ((sample[0] > 0.065f) && (sample[0] < 0.150f)
-				&& 	(sample[1] > 0.065f) && (sample[1] < 0.150f)
-				&& 	(sample[2] > 0.065f) && (sample[2] < 0.150f)
-			) {
-			color = "grey";
-//			System.out.println("DETECTING WHITE");
-		} else {
-//			System.out.println("DETECTING RGB VALUES   " + sample[0] + "/" + sample[1] + "/" + sample[2]);
-			color = "unknown";
-		}
-	
-	
-	
-	return color;
-}
-
-
-// OLD CODE MIGHT REUSE AT SOME POINT BUT NOT ATM 
-/////////////////////////////////////////////////////////////////////////////
-// Touch sensors currently not in use, as we can make the robot run without pressing a button.
-//touch1.fetchSample(sample1, 0);
-//touch2.fetchSample(sample2, 0);
-
-//String color = determineColor(colorSample);
-
-// Run motor spins wheels, and checks what the color is. If a certain color, does a certain thing.
-// Red stops motor, Black makes it run, White makes it slow down, and unknown color makes it spin to a halt.
-
-// error multiplier
-
-// if pressing button 1, set speed of wheels to 2 RPS, and write to screen.
-//if (sample1[0] != 0) {
-//	
-//	// COLOR ID MODE CHANGED TO RGB MODE, NOT USING ANYMORE.
-//	// Colors have IDs in ColorIDMode. Colors with IDs are:
-//	// 0 1 2 3 4 5 6 7
-//	// None, Black, Blue, Green, Yellow, Red, White, Brown
-//	// If the color detected is not red, run motor.
-//	
-//	
-//	colorProvider.fetchSample(colorSample, 0);
-//	String color = determineColor(colorSample);
-//	
-//	if (color == "red") {
-//		mA.stop();
-//		mB.stop();
-//	} else if (color == "black") {
-//		runMotor(mA);
-//		runMotor(mB);
-//	} else if (color == "white") {
-//		runMotor(mA);
-//		runMotor(mB);
-//	} else if (color == "unknown") {
-//		mA.stop();
-//		mB.stop();
-//	}
-//		
-//	
-//} 
-//// if pressing button 2, fetch the values in the colorprovider, and print them out on the screen. These are normalised between 0 and 1.
-//else if (sample2[0] != 0) {
-//	
-//	
-//	colorProvider.fetchSample(colorSample, 0);
-//	// RED STRIP
-//	// Red 0.23431373
-//	// Green 0.46078432
-//	// Blue 0.033333335
-//	
-//	// BLACK STRIP
-//	// Red 0.0029411765
-//	// Green 0.004901961
-//	// Blue 0.003921569
-//	
-//	// WHITE STRIP
-//	// Red 0.27058825
-//	// Green 0.2617647
-//	// Blue 0.17843138
-//	
-//	// ELSE OTHER COLOR, WE DONT NEED TO TEST FOR OTHERS FOR NOW
-//	
-//	
-//	
-////	System.out.println(colorSample[0]);
-////	System.out.println(colorSample[1]);
-////	System.out.println(colorSample[2]);
-//	
-////	if ( 	(colorSample[0] > 0.230f) && (colorSample[0] < 0.240)
-////		&&	(colorSample[1] > 0.455) && (colorSample[1] < 0.470)
-////		&& 	(colorSample[2] > 0.030) && (colorSample[2] < 0.040)
-////		) {
-////		System.out.println("DETECTING RED");
-////	} else if ( (colorSample[0] > 0.02) && (colorSample[0] < 0.04)
-////			&& 	(colorSample[1] > 0.04) && (colorSample[1] < 0.06)
-////			&& 	(colorSample[2] > 0.03) && (colorSample[2] < 0.05)
-////		) {
-////		System.out.println("DETECTING BLACK");
-////	} else if ((colorSample[0] > 0.265) && (colorSample[0] < 0.275)
-////			&& 	(colorSample[1] > 0.255) && (colorSample[1] < 0.265)
-////			&& 	(colorSample[2] > 0.170) && (colorSample[2] < 0.185)
-////		) {
-////		System.out.println("DETECTING WHITE");
-////	} else {
-////		System.out.println("DETECTING RGB VALUES   " + colorSample[0] + "/" + colorSample[1] + "/" + colorSample[2]);
-////	}
-////
-//	// Print out pressing button 2 and hue of red in rgb spectrum.
-////	g.drawString("PRESSING BUTTON 2 " +  colorSample[0], 0, 0, GraphicsLCD.VCENTER | GraphicsLCD.LEFT);
-//	
-//	
-//} else {
-//	
-//	mA.stop();
-//	mB.stop();
-//	
-//}
-//touch2.fetchSample(sample2, 0);
-//if (sample2[0] != 0) {
-//	g.clear();
-//	mB.setSpeed(90);
-//	mB.setSpeed(90);
-//	mB.forward();
-//	g.drawString("Pressing B", 0, 20, GraphicsLCD.VCENTER | GraphicsLCD.LEFT);
-//} else {
-//	mB.stop();
-//}
-//
-//gyro.fetchSample(gyroTest, 0);
-//int valueTest = (int)gyroTest[0];
-//
-//g.clear();
-//g.drawString("Gyro Angle = "  + Integer.toString(valueTest), 0, 50, GraphicsLCD.VCENTER | GraphicsLCD.LEFT);
-
-//g.setAutoRefresh(true);
-
-
-// Gyro sensor
-//EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S3);
-//SampleProvider gyro = gyroSensor.getAngleMode();
-//float[] gyroTest = new float[gyro.sampleSize()];
-
-//			if (color == "red") {
-//	mA.stop();
-//	mB.stop();
-//} else if (color == "black") {
-//	mA.startSynchronization();
-//	runMotor(mA, color);
-//	runMotor(mB, color);
-//	mA.endSynchronization();
-//} else if (color == "white") {
-//	runMotor(mA, color);
-//	runMotor(mB,color);
-//} else if (color == "unknown") {
-//	mA.startSynchronization();
-//	mA.stop(true);
-//	mB.stop();
-//	mA.endSynchronization();
-//}
-
 
 }
