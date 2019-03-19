e6ae819c1ffc49907ce3e463f69829022cbd11e5
AmarKaurD <43579877+AmarKaurD@users.noreply.github.com>
2019-03-19 18:29:10 +0000

    Add files via upload

--- newLegoProject2/src/newLegoProject2/Grid.java
+++ newLegoProject2/src/newLegoProject2/Grid.java
@@ -3,6 +3,7 @@ package newLegoProject2;
 import java.util.ArrayList;
 
 public class Grid {
+
 	private int scale;
 	private Obj rob;
 	private ArrayList<Obj> objs;
@@ -19,35 +20,46 @@ public class Grid {
 		return scale;
 	}
 	
-	private boolean can_move(Obj o)
+	public boolean can_move(Obj o)
 	{
-		if (o.get_x() < scale && o .get_y() < scale)
+		if ((o.get_move_x() <= scale) && (o.get_move_x() > -1) && (o.get_move_y() <= scale) && (o.get_move_y() > -1)) 
 			return true;
 		return false;
 	}
 	
+	// Checks if the next move is safe to make (i.e doesn't collide with obstacle)
 	private boolean collision()
 	{
 		for (Obj o: rob.get_obs()) {
-			if (o.get_x())
+			if (rob.intersect(o))
+			    return true;
 		}
+		
+		return false;
 	}
 	
+	// Move every object on the grid if they can move
 	public void take_turn() 
 	{
-		if (can_move(rob))
-			rob.move(1);
+		if (can_move(rob)){
+			Obj goal = rob.get_goal();
+			double ang = (rob.calc_angle(goal));
+			if (ang != 0) {
+				rob.set_rotated(true);
+				rob.set_angle(ang);
+			}
+			rob.move();
+		}
 		else
 			System.out.println("hit wall!");
 		
-		for (Obj o: objs)
-			o.move(0);
+		/*for (Obj o: objs)
+			o.move();*/
 	}
 	
 	public void add_obj(Obj o) {
 		if ((o.get_x() < scale && o.get_x() > 0) &&
 			(o.get_y() < scale && o.get_y() > 0))
 			objs.add(o);
-//			System.out.println(o.toString());
 	}
 }
